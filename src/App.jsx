dbb29399005c5704259bac6ac723c66d3aad4632
Abey Joy <abeyjoy@Abeys-MBP.home>
2017-04-05 01:41:56 -0400

    Finished basic Board functionality: Edit and Remove

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,46 @@ import Comment from './components/Comment_Component.jsx'
 import Checkbox from './components/Checkbox_Component.jsx'
 
 var Board = React.createClass({
+
+	getInitialState: function(){
+		return{
+			comments:[
+				'I like Bacon',
+				'Ice Cream is good',
+				'Beans are okay'
+			]
+
+		}
+	},
+
+	addComment: function(text){
+		var arr = this.state.comments;
+		arr.push(text);
+		this.setState({comments: arr});
+	},
+
+	removeComment: function(i){
+		var arr = this.state.comments;
+		arr.splice(i,1);
+		this.setState({comments: arr});
+	},
+
+	updateComment: function(newText,i){
+		var arr = this.state.comments;
+		arr[i] = newText;
+		this.setState({comments: arr});
+	},
+
+	eachComment: function(text, i){
+		return(
+			<Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>{text}</Comment>
+		);
+	},
+
 	render: function(){
-		return (<div> 
-					<Comment>This is item ONE.</Comment>
-					<Comment>This is item TWO.</Comment>
-					<Comment>This is item THREE.</Comment>
+		return (<div>
+				<button onClick={this.addComment.bind(null,'Default Text')}>Add New</button>
+				{this.state.comments.map(this.eachComment)}
 					<Checkbox />
 				</div>
 			)
--- src/components/Comment_Component.jsx
+++ src/components/Comment_Component.jsx
@@ -2,22 +2,55 @@ import React, {Component} from "react"
 
 var Comment = React.createClass({
 
+	getInitialState: function(){
+		return{
+			editing: false
+		}
+	},
+
 	edit: function(){
-		alert("Editing Comment");
+		this.setState({editing: true});
+
 	},
 
 	remove: function(){
-		alert("Removing Comment");
+		console.log("Removing Comment");
+		this.props.deleteFromBoard(this.props.index);
 	},
 
-	render: function(){
-		return(
+	save: function(){
+		var val = this.refs.newText.value;
+		this.props.updateCommentText(val,this.props.index);
+		this.setState({editing: false});
+	},
+
+	renderNormal: function(){
+        return(
 			<div>
 				<h1>{this.props.children} </h1>
 				<button onClick={this.edit}> Edit </button>
 				<button onClick={this.remove}> Delete </button>
 			</div>
-			)
+        )
+	},
+
+	renderForm: function(){
+        return(
+			<div>
+				<textarea ref="newText" defaultValue={this.props.children}></textarea>
+				<button onClick={this.save}> Save </button>
+
+			</div>
+        )
+	},
+
+	render: function(){
+
+			if(this.state.editing){
+				return this.renderForm();
+            }else{
+				return this.renderNormal();
+            }
 	}
 });
 
