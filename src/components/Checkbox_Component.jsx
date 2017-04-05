364b161669ae56d0cacb04eed429d7ecfa978297
Abey Joy <abeyjoy@Abeys-MBP.home>
2017-04-05 00:33:57 -0400

    added checkbox component

--- src/components/Checkbox_Component.jsx
+++ src/components/Checkbox_Component.jsx
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
 var Checkbox = React.createClass({
+    getInitialState: function(){
+        return{
+            checked: false
+        }
+    },
+
+    changeChecked: function(){
+        this.setState({checked: !this.state.checked})
+    },
 
     render: function(){
+        var message;
+        if(this.state.checked){
+            message = "I am checked!";
+        }else{
+            message = "I am not checked. Check Me!";
+        }
         return (
             <div>
-                <h1> Check Me! </h1>
+                <h1> {message} </h1>
+                <input type="checkbox" onChange={this.changeChecked} />
             </div>
         )
     }
